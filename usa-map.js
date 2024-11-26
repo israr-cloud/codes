46aa4ea42c328c43dfdce5659a8e68a51863a6ab
israr-cloud <israrahmedchk52@gmail.com>
2024-11-26 17:49:04 +0500

    Update usa-map.js

--- usa-map.js
+++ usa-map.js
@@ -1,1 +1,12 @@
 
+document.addEventListener("DOMContentLoaded", function() {
+    // The HTML content to be added
+    let htmlContent = ` 
+    
+    `;
+
+ // Select the div with the class .usa-map-wrapper
+    let targetDiv = document.querySelector('.usa-map-wrapper');
+
+    targetDiv.innerHTML += htmlContent;
+});
